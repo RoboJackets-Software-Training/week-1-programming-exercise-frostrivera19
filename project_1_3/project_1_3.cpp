fb5562156aab2def8bf294a65022a66d0f95bf64
Faris Durrani <afarisdurrani@gmail.com>
2021-05-26 22:40:35 -0400

    add space

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -26,7 +26,7 @@ void printVector(std::vector<double> vec)
   std::cout << "{";
   for (int i = 0; i < vec.size() - 1; i++)
   {
-    std::cout << vec[i] << ",";
+    std::cout << vec[i] << ", ";
   }
   std::cout << vec[vec.size() - 1] << "}" << std::endl;
 }
